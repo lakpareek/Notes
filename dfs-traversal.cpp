b581b506b1dc817e39f9c0b3ebce7fae095f9577
lakpareek <126066065+lakpareek@users.noreply.github.com>
2023-06-07 20:08:54 +0530

    dfs-traversal.cpp

--- dfs-traversal.cpp
+++ dfs-traversal.cpp
@@ -30,3 +30,46 @@ vector<vector<int>> depthFirstSearch(int vertex, int E, vector<vector<int>> &edg
     }
     return ans;
 }
+
+
+/*The specific implementation of the depth-first search (DFS) function can vary depending on the requirements and preferences of the platform or problem you are working with. Both forms, vector<vector<int>> and vector<int>, are valid and can be used depending on the desired output.
+
+In the above code, the DFS function is implemented to return a vector<vector<int>>. This means that it will find all the connected components in the graph and return them as separate vectors, where each inner vector represents a connected component.
+
+However, if you prefer a different form where the DFS function returns a single vector that contains all the nodes visited during the traversal, you can modify the code accordingly.
+
+Here's an example of how you can modify the code to return a single vector instead of a vector of vectors:*/
+
+
+void helper(unordered_map<int, list<int>>& adj, int node, vector<bool>& visited, vector<int>& nodes) {
+    visited[node] = true;
+    nodes.push_back(node);
+
+    for (auto i : adj[node]) {
+        if (!visited[i]) {
+            helper(adj, i, visited, nodes);
+        }
+    }
+}
+
+vector<int> depthFirstSearch(int V, int E, vector<vector<int>>& edges) {
+    unordered_map<int, list<int>> adj;
+    for (int i = 0; i < E; i++) {
+        int u = edges[i][0];
+        int v = edges[i][1];
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    vector<bool> visited(V, false);
+    vector<int> nodes;
+
+    for (int i = 0; i < V; i++) {
+        if (!visited[i]) {
+            helper(adj, i, visited, nodes);
+        }
+    }
+
+    return nodes;
+}
